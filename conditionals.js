bf53b7e47e2e0bde36082cf1af5d76f1c01fcca5
rowemeister83 <arworkrelatedmail@gmail.com>
2022-02-01 14:23:38 +0000

    try the conditionals

---
+++ conditionals.js
@@ -0,0 +1,11 @@
+'use strict'
+
+let a = 2;
+
+if(a == 0 ) {
+    console.log("sunshine");
+    } else if (a ! =2) {
+    console.log("moonlight");    
+    } else {
+    console.log('boogie')
+    }
