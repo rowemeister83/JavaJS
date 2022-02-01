e430f76514bb5112a013b06fcf3775f4b9ac1f17
rowemeister83 <arworkrelatedmail@gmail.com>
2022-02-01 10:02:35 +0000

    initial follow-along code

---
+++ script.js
@@ -0,0 +1,19 @@
+'use strict'
+
+//let let =2;
+
+console.log("Hellow World!");
+
+let text = "this is a message";
+
+console.log(text);
+
+console.info(text);
+
+console.warn(text);
+
+console.error(text);
+
+console.log('%c' + text, 'color:black; background-color:white; font-size: 20pxt; padding: 10px;');
+
+
