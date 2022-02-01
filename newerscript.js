2f963c1c9899cd45db7ac0e9de48ac7415204fa0
rowemeister83 <arworkrelatedmail@gmail.com>
2022-02-01 11:42:57 +0000

    entered js script updates

---
+++ newerscript.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// Automatic
+// Semi-cOLON
+// Insertion
+
+let a = 1;
+let b = 1;
+
+a++;b;
+
+console.log("a = ${a}");
+console.log("b = ${b}");
---
+++ qacomm-js-solutions.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let a;
+let b = "12345";
+let c = 12344;
+let d = true;
+let e = {a:"JavaScript"};
+
+typeof(a);
+
+typeof(b);
+
+typeof(c);
+
+typeof(d);
+
+typeof(e);
+
+////// 
+
+let totalMoney = 4000;
+
+let moneyPaidSoFar = 2348;
+
+let totalLeftToPay = totalMoney - moneyPaidSoFar;
+
+console.log(`The total price amounts to £${totalMoney} the remainder due is £${totalLeftToPay}`);
