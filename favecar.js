c865148e8e3425b1dd6af2344114b9f2e75ea218
rowemeister83 <arworkrelatedmail@gmail.com>
2022-02-02 07:32:45 +0000

    corrected mistakes using node.js in vcs terminal

--- favecar.js
+++ favecar.js
@@ -1,6 +1,6 @@
 'use strict'
 
-let Car = "Williams";
+let Car = "Williams,";
 let Model = "the FW14B";
 
-console.log("My favourite alltime Car is a: " + Car + "and their chassis from the 1992 F1 Season: " + Model);
+console.log("My favourite alltime Car is a " + Car +  " and their chassis from the 1992 F1 Season: " + Model);
